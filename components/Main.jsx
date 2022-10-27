f0f4de633832442bdcee43c7dace813816434f09
Prathap <prathap.sims@gmail.com>
2022-10-26 21:12:50 -0700

    fix sizing issue

--- components/Main.jsx
+++ components/Main.jsx
@@ -15,10 +15,10 @@ const Main = () => {
           <p className='uppercase text-sm tracking-widest text-gray-800'>
             LET&apos;S BUILD SOMETHING TOGETHER
           </p>
-          <h2 className='py-4 text-gray-800'>
+          <h2 className='py-2 text-gray-800'>
               Hi, I&apos;m <span className='text-[#5651e5]'>Prathap</span>
           </h2>
-          <h2 className='py-4 text-gray-800'>
+          <h2 className='py-2 text-gray-800'>
                 A Full-stack Web Developer
           </h2>
           <p className='py-4 text-gray-700 max-w-[70%] m-auto'>
