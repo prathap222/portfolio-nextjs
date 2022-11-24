a26c22104bf5f34a6f756acfd37cbaed7faac014
Prathap <prathap.sims@gmail.com>
2022-11-23 23:36:15 -0800

    Add icons

--- components/Main.jsx
+++ components/Main.jsx
@@ -10,12 +10,12 @@ import Image from 'next/image';
 const Main = () => {
   return (
     <div id='home' className='w-full h-screen text-center '>
-      <div className='max-w-[1240px] w-full h-full mx-auto py-16  flex justify-center items-center'>
+      <div className='max-w-[1240px] w-full h-[130%] mx-auto p-2  flex justify-center items-center'>
         <div>
-          <p className='uppercase text-sm tracking-widestw-full h-full mx-auto py-16 text-[#eeeef8]'>
+          <p className='uppercase text-sm tracking-widest  text-[#eeeef8]'>
             LET&apos;S BUILD SOMETHING TOGETHER
           </p>
-          <h2 className='py-2 text-[#eeeef8]'>
+          <h2 className='py-4 text-[#eeeef8]'>
               Hi, I&apos;m <span className='text-[#5651e5]'>Prathap</span>
           </h2>
           <h2 className='py-2 text-[#eeeef8]'>
@@ -40,7 +40,7 @@ const Main = () => {
               target='_blank'
               rel='noreferrer'
             >
-              <div className='rounded-full bg-[#7B68EE] shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <div className='rounded-full bg-[#7B68EE] shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 animate-pulse'>
                 <FaGithub/>
               </div>
             </a>
@@ -54,14 +54,14 @@ const Main = () => {
               target='_blank'
               rel='noreferrer'
             >
-              <div className='rounded-full bg-[#7B68EE] shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <div className='rounded-full bg-[#7B68EE] shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 animate-pulse'>
                 <BsFillPersonLinesFill />
               </div>
             </a>
 
           </div>
 
-          <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96 shadow-lg shadow-gray-200 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+          <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96 shadow-lg shadow-gray-200 p-6 cursor-pointer hover:scale-110 ease-in duration-300  ">
             <Image src={ProfileImg} layout="fill" objectFit="cover" />
           </div>
 
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -47,32 +47,65 @@ const Navbar = () => {
 
   return (
     <div style={{background:`${navBg}`}} className={shadow ? 'fixed w-full h-20 shadow-xl z-[100]' : 'fixed w-full h-20  z-[100]'}>
-      <div className='flex justify-between item-center w-full h-full px-2 2xl:px-16 '>
-        {/* <Link href='/'>
+      <div className='flex justify-between item-center w-full h-full px-2 2xl:px-16  '>
+        <Link href='/'>
           <Image src={NavLogo} alt="/" width='125' height='50' />
-        </Link> */}
+        </Link>
         
         <div>
           <ul style={{color: `${linkColor}`}} className='hidden md:flex'>
             <Link href='/'>
               {/* <li className='ml-10 text-sm uppercase hover:border-b'>Home</li> */}
-              <button className='px-3 py-2 mt-4 mr-8 cursor-pointer hover:scale-110 ease-in duration-300 ' >Home</button>
+              <button className='px-3 py-2 mt-4 mr-8 cursor-pointer hover:scale-110 ease-in duration-300 animate-pulse' >Home
+                <span>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                  </svg>
+
+                </span>
+              </button>
             </Link>
             <Link href='/#about'>
               {/* <li className='ml-10 text-sm uppercase hover:border-b'>About</li> */}
-              <button className='px-3 py-2 mt-4 mr-8 cursor-pointer hover:scale-110 ease-in duration-300'>About</button>
+              <button className='px-3 py-2 mt-4 mr-8 cursor-pointer hover:scale-110 ease-in duration-300 animate-pulse'>About
+                <span>
+                  <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                  </svg>
+
+                </span>
+              </button>
             </Link>
             <Link href='/#skills'>
               {/* <li className='ml-10 text-sm uppercase hover:border-b'>Skills</li> */}
-              <button className='px-3 py-2 mt-4 mr-8 cursor-pointer hover:scale-110 ease-in duration-300'>Skills</button>
+              <button className='px-3 py-2 mt-4 mr-8 cursor-pointer hover:scale-110 ease-in duration-300 animate-pulse'>Skills
+                <span>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M9.53 16.122a3 3 0 00-5.78 1.128 2.25 2.25 0 01-2.4 2.245 4.5 4.5 0 008.4-2.245c0-.399-.078-.78-.22-1.128zm0 0a15.998 15.998 0 003.388-1.62m-5.043-.025a15.994 15.994 0 011.622-3.395m3.42 3.42a15.995 15.995 0 004.764-4.648l3.876-5.814a1.151 1.151 0 00-1.597-1.597L14.146 6.32a15.996 15.996 0 00-4.649 4.763m3.42 3.42a6.776 6.776 0 00-3.42-3.42" />
+                  </svg>
+
+                </span>
+              </button>
             </Link>
             <Link href='/#projects'>
               {/* <li className='ml-10 text-sm uppercase hover:border-b'>Projects</li> */}
-              <button className='px-3 py-2 mt-4 mr-8 cursor-pointer hover:scale-110 ease-in duration-300'>Projects</button>
+              <button className='px-3 py-2 mt-4 mr-8 cursor-pointer hover:scale-110 ease-in duration-300 animate-pulse'>Projects
+                <span>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                  </svg>
+
+                </span>
+              </button>
             </Link>
             <Link href='/#contact'>
               {/* <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li> */}
-              <button className='px-3 py-2 mt-4 mr-8 cursor-pointer hover:scale-110 ease-in duration-300'>Contact</button>
+              <button className='px-3 py-2 mt-4 mr-8 cursor-pointer hover:scale-110 ease-in duration-300 animate-pulse'>Contact
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 01-.825-.242m9.345-8.334a2.126 2.126 0 00-.476-.095 48.64 48.64 0 00-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0011.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
+                </svg>
+
+              </button>
             </Link>
           </ul>
           <div onClick={handleNav} className='md:hidden'>
@@ -86,12 +119,12 @@ const Navbar = () => {
         <div className={nav ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500' : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'}
         >
           <div>
-            <div  className='flex w-full items-center justify-between'>
+            <div  className='flex w-full items-center justify-between '>
               <Link href='/'>
                 <Image src={NavLogo} width='87' height='35' alt='/'/>
               </Link>
               
-              <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+              <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer '>
                 <AiOutlineClose/>
               </div>
             </div>
